4d8ba900ccf3f560d72b00126fd3481c7fd86b1c
Ankit Gupta <ankitgupta3466@gmail.com>
2019-08-25 10:34:16 +0530

    text to speech

---
+++ text_speech.py
@@ -0,0 +1,6 @@
+import pyttsx3
+
+s=pyttsx3.init()
+
+s.say('Hello this is ak')
+s.runAndWait()
---
+++ translator.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+from textblob import TextBlob
+import goslate
+
+s = sr.Recognizer()
+
+print(sr.__version__)
+#print(sr.Microphone.list_microphone_names())
+
+with sr.Microphone() as source:
+	print('Say something...')
+	audio = s.listen(source)
+	text = s.recognize_google(audio)
+
+print('Origin Language : ')
+print(text)
+
+word = TextBlob(text)
+print('Translated language : ')
+print(word.translate(from_lang='en' , to='hi'))
+
+# Another method
+gs = goslate.Goslate()
+print(gs.translate(text,'hi'))
+print(gs.translate(text,'es'))
